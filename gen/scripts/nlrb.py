085ae9c5e07be0be9d99b10b498676c4651d7015
Kevin <kevin.reuning@gmail.com>
2021-05-25 20:48:03 -0400

    increasing implicit wait time [skip actions]

--- gen/scripts/nlrb.py
+++ gen/scripts/nlrb.py
@@ -27,13 +27,15 @@ options.add_argument("--disable-dev-shm-usage")
 
 browser = webdriver.Chrome(options=options)
 browser.set_page_load_timeout(-1)
-browser.implicitly_wait(30)
+browser.implicitly_wait(120)
 
 
 print("Opening Page")
 browser.get(r'https://www.nlrb.gov/search/case?f[0]=case_type:R&s[0]=Open')
 browser.find_element_by_id("download-button").click()
+
 link = browser.find_element_by_link_text("Cases (All Dates)")
+
 file_url = link.get_attribute("href")
 file = requests.get(file_url)
 print("File Downloaded")
