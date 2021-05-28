10c772407e263dbc07305a9fb941a5ad484525dc
Kevin <kevin.reuning@gmail.com>
2021-05-27 21:01:26 -0400

    Setting up multiple tries [skip actions]

--- gen/scripts/nlrb.py
+++ gen/scripts/nlrb.py
@@ -25,21 +25,33 @@ options.add_argument("--headless")
 options.add_argument("--no-sandbox")
 options.add_argument("--disable-dev-shm-usage")
 
-browser = webdriver.Chrome(options=options)
-browser.set_page_load_timeout(-1)
-browser.implicitly_wait(120)
+def get_data(chrome_options):
+    browser = webdriver.Chrome(options=chrome_options)
+    browser.set_page_load_timeout(-1)
+    browser.implicitly_wait(120)
 
 
-print("Opening Page")
-browser.get(r'https://www.nlrb.gov/search/case?f[0]=case_type:R&s[0]=Open')
-browser.find_element_by_id("download-button").click()
+    print("Opening Page")
+    browser.get(r'https://www.nlrb.gov/search/case?f[0]=case_type:R&s[0]=Open')
+    browser.find_element_by_id("download-button").click()
 
-link = browser.find_element_by_link_text("Cases (All Dates)")
+    link = browser.find_element_by_link_text("Cases (All Dates)")
 
-file_url = link.get_attribute("href")
-file = requests.get(file_url)
-print("File Downloaded")
+    file_url = link.get_attribute("href")
+    file = requests.get(file_url)
+    print("File Downloaded")
 
-with open(download_folder + '/new_open_data.csv','wb') as f:
-  f.write(file.content)
-print("File Saved")
+    with open(download_folder + '/new_open_data.csv','wb') as f:
+      f.write(file.content)
+    print("File Saved")
+
+    return True
+
+check = 0
+while check < 5:
+    try:
+        print("Attempt " + str(check + 1))
+        get_data(chrome_options = options)
+        check = 6
+    except:
+        check += 1
