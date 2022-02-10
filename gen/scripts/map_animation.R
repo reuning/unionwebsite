2bbe67938ebb76006800942987b580e75fa0da14
Kevin <kevin.reuning@gmail.com>
2022-02-10 09:04:11 -0500

    Made a gif

---
+++ gen/scripts/map_animation.R
@@ -0,0 +1,213 @@
+library(tidygeocoder)
+library(here)
+library(sf)
+library(rnaturalearth)
+library(rnaturalearthdata)
+library(rnaturalearthhires)
+library(rgeos)
+
+source(here("gen", "scripts", "Scripts.R"))
+
+## Based on https://r-spatial.org/r/2018/10/25/ggplot2-sf-3.html
+
+dt <- fread(here("gen", "data", "recent_election_results.csv"))
+
+open_dt <- dt[grepl("starbuck", `Case Name`, ignore.case = T)]
+
+open_dt <- unique(open_dt, by = c("Case", "City", "State"))
+
+cols <- c(as.vector(which(apply(open_dt, 2,
+                                function(x)
+                                  all(!is.na(x))))), 32:42)
+coded_dt <-
+  fread(here("gen", "data", "elections_with_location.csv"))
+tmp <- unique(coded_dt[, .(Case, `Case Name`, lat, 
+                           long, formatted_address, 
+                           accuracy, accuracy_type, 
+                           source, address_components.city, 
+                           address_components.county, 
+                           address_components.state, 
+                           address_components.zip, 
+                           address_components.country,
+                           seached, 
+                           lat_jit, 
+                           long_jit)])
+full_dt <- merge(open_dt, tmp, all.x = T, by=c("Case", "Case Name"))
+
+if (any(is.na(full_dt$seached))) {
+  to_code_dt <- full_dt[is.na(seached)]
+  done_dt <- full_dt[!is.na(seached)]
+  
+  to_code_dt <- tidygeocoder::geocode(
+    as.data.frame(to_code_dt[,1:Voters]),
+    city = "City",
+    state = "State",
+    full_results = T,
+    # return_type = "geographies",
+    method = "geocodio"
+  )
+  
+  try(to_code_dt$address_components.street <- NULL)
+  try(to_code_dt$address_components.formatted_street <- NULL)
+  try(to_code_dt$address_components.suffix <- NULL)
+  try(to_code_dt$address_components.predirectional <- NULL)
+  try(to_code_dt$address_components.postdirectional <- NULL)
+  
+  to_code_dt$seached <- 1
+  to_code_dt$lat_jit <- to_code_dt$lat + runif(nrow(to_code_dt), -.5, .5)
+  to_code_dt$long_jit <- to_code_dt$long + runif(nrow(to_code_dt), -.5, .5)
+  
+  
+  full_dt <- rbind(done_dt, to_code_dt)
+}
+
+full_dt[,Election_Data :=ifelse(`Tally Date`=="", "No", "Yes") ]
+
+
+
+
+usa <- ne_states(returnclass = 'sf',
+                 country = c("united states of america", 
+                             "puerto rico"))
+full_dt <- prep_data(full_dt)
+# full_dt <- full_dt[grepl("RC", Case)]
+# full_dt[, Date := as.Date(`Date Filed`, format = "%m/%d/%Y")]
+full_dt <- full_dt[Case_Type == "RC"]
+st_dt <- full_dt[grepl("starbuck", Case_Name, ignore.case = T)]
+
+
+site <- st_as_sf(
+  unique(st_dt[!is.na(lat)], by = "Case"),
+  coords = c("long_jit", "lat_jit"),
+  crs = 4326,
+  agr = "constant"
+)
+
+library(gganimate)
+# tween_sf()
+
+# library(transformr)
+mainland <- ggplot(data = usa) +
+  geom_sf(fill = "white") + 
+  theme_void() + 
+  # theme_void(base_family = "Crimson Pro")  +
+  geom_sf(
+    data = site,
+    # shape = 21,
+    color = "orangered3",
+    size = 4, alpha=.7,
+    aes(group=Case) 
+    # aes(fill = size, shape=Election_Data)
+  ) +
+  transition_time(Date_Filed)+ 
+  shadow_mark() + 
+  enter_grow() +
+  # exit_recolor(color = "orangered3") +
+  ggtitle("Union Filings at Starbucks", subtitle = "Date: {frame_time}") + 
+  labs(caption = "Location is approximate based on cities. Each is jittered so overlapping cases are obvious.") +
+  coord_sf(
+    crs = st_crs(2163),
+    xlim = c(-2500000, 2500000),
+    ylim = c(-2300000,  730000)
+  ) +
+  NULL
+
+# mainland
+
+alaska <- ggplot(data = usa) +
+  geom_sf(fill = "white") + 
+  theme_void() + 
+  # theme_void(base_family = "Crimson Pro")  +
+  geom_sf(
+    data = site,
+    # shape = 21,
+    color = "orangered3",
+    size = 4, alpha=.7,
+    # aes(fill = size, shape=Election_Data)
+  ) + 
+  guides(fill = F, shape=F) +
+  coord_sf(
+    crs = st_crs(3467),
+    xlim = c(-2400000, 1600000),
+    ylim = c(200000, 2500000),
+    expand = FALSE,
+    datum = NA
+  )
+
+
+puerto <- ggplot(data = usa) +
+  geom_sf(fill = "white") + 
+  theme_void() + 
+  # theme_void(base_family = "Crimson Pro")  +
+  geom_sf(
+    data = site,
+    # shape = 21,
+    color = "orangered3",
+    size = 4, alpha=.7,
+    # aes(fill = size, shape=Election_Data)
+  ) +
+  guides(fill = F, shape=F) +
+  coord_sf(
+    crs = st_crs(3991),
+    xlim = c(-50000, 1000000),
+    ylim = c(0, 260000),
+    expand = FALSE,
+    datum = NA
+  )
+
+# puerto
+
+hawaii  <- ggplot(data = usa) +
+  geom_sf(fill = "white") +  
+  theme_void() + 
+  # theme_void(base_family = "Crimson Pro")  +
+  geom_sf(
+    data = site,
+    # shape = 21,
+    color = "orangered3",
+    size = 4, alpha=.7,
+    # aes(fill = size, shape=Election_Data)
+  )  + 
+  # scale_fill_date(name = "Filing\nDate", 
+  #                 low = "#0072B2",
+  #                 high = "#D55E00") +
+  guides(fill = F, shape=F) +
+  coord_sf(
+    crs = st_crs(4135),
+    xlim = c(-161,-154),
+    ylim = c(18, 23),
+    expand = FALSE,
+    datum = NA
+  )
+
+p <- mainland +
+  annotation_custom(
+    grob = ggplotGrob(alaska),
+    xmin = -2750000,
+    xmax = -2750000 + (1600000 - (-2400000)) / 2.5,
+    ymin = -2450000,
+    ymax = -2450000 + (2500000 - 200000) / 2.5
+  ) +
+  annotation_custom(
+    grob = ggplotGrob(hawaii),
+    xmin = -1250000,
+    xmax = -1250000 + (-154 - (-161)) * 120000,
+    ymin = -2450000,
+    ymax = -2450000 + (23 - 18) * 120000
+  ) +
+  annotation_custom(
+    grob = ggplotGrob(puerto),
+    xmin = 750000,
+    xmax = 750000 + (1000000 - (-50000)) / 2,
+    ymin = -2250000,
+    ymax = -2250000 + (260000 - 0) /2
+  ) 
+
+# p
+animate(p, end_pause=10, height=4, width=6, units="in", res=150)
+anim_save("~/Desktop/starbucks_filings.gif")
+
+# ggsave(here("content", "data",  "starbucks", "map.png"),
+#        height=6, width=10, type = "cairo",
+#        units="in", dpi=200)
+
