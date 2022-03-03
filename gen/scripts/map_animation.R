39e718bda9e0cead34a95af388ee0690b8ac5d98
Kevin <kevin.reuning@gmail.com>
2022-03-03 08:11:39 -0500

    map fixes

--- gen/scripts/map_animation.R
+++ gen/scripts/map_animation.R
@@ -5,6 +5,9 @@ library(rnaturalearth)
 library(rnaturalearthdata)
 library(rnaturalearthhires)
 library(rgeos)
+library(tidyr)
+library(dplyr)
+library(gganimate)
 
 source(here("gen", "scripts", "Scripts.R"))
 
@@ -62,8 +65,8 @@ if (any(is.na(full_dt$seached))) {
 }
 
 full_dt[,Election_Data :=ifelse(`Tally Date`=="", "No", "Yes") ]
-
-
+full_dt[City == "Amherst" & State == "NY", lat_jit:=42.978333]
+full_dt[City == "Amherst" & State == "NY", long_jit:=-78.8 ]
 
 
 usa <- ne_states(returnclass = 'sf',
@@ -75,139 +78,372 @@ full_dt <- prep_data(full_dt)
 full_dt <- full_dt[Case_Type == "RC"]
 st_dt <- full_dt[grepl("starbuck", Case_Name, ignore.case = T)]
 
+sub_dt <- st_dt[,.(Case, Date_Filed, Date_Closed, Reason_Closed, long_jit, lat_jit, State)]
+
+sub_dt <- melt(sub_dt, measure = patterns("Date_"), na.rm = T )
+sub_dt[variable=="Date_Filed", Reason_Closed:="Filed"]
+sub_dt[Reason_Closed=="Withdrawal Non-adjusted", Reason_Closed:="Withdrawn" ]
+sub_dt[Reason_Closed=="Certific. of Representative", Reason_Closed:="Voted to Unionize" ]
+names(sub_dt)[2] <- "Status"
+ 
+# site <- st_as_sf(
+#   unique(sub_dt[!is.na(lat_jit)], by = c("Case", "Status")),
+#   coords = c("long_jit", "lat_jit"),
+#   crs = 4326,
+#   agr = "constant"
+# )
+# library(gganimate)
+# # tween_sf()
+# 
+# # library(transformr)
+# mainland <- ggplot(data = usa) +
+#   geom_sf(fill = "white") + 
+#   theme_void() + 
+#   # theme_void(base_family = "Crimson Pro")  +
+#   geom_sf(
+#     data = site,
+#     # shape = 21,
+#     size = 4, alpha=.7,
+#     aes(group=Case, 
+#         color=Status) 
+#     # aes(fill = size, shape=Election_Data)
+#   ) +
+#   transition_time(value)+ 
+#   shadow_mark() + 
+#   enter_grow() +
+#   # exit_recolor(color = "orangered3") +
+#   ggtitle("Union Filings at Starbucks", subtitle = "Date: {frame_time}") + 
+#   labs(caption = "Location is approximate based on cities. Each is jittered so overlapping cases are obvious.") +
+#   coord_sf(
+#     crs = st_crs(2163),
+#     xlim = c(-2500000, 2500000),
+#     ylim = c(-2300000,  730000)
+#   ) +
+#   NULL
+# 
+# # mainland
+# 
+# alaska <- ggplot(data = usa) +
+#   geom_sf(fill = "white") + 
+#   theme_void() + 
+#   # theme_void(base_family = "Crimson Pro")  +
+#   geom_sf(
+#     data = site,
+#     # shape = 21,
+#     size = 4, alpha=.7,
+#     aes(group=Case, 
+#         color=Status) 
+#   ) + 
+#   guides(fill = F, shape=F, color=F) +
+#   coord_sf(
+#     crs = st_crs(3467),
+#     xlim = c(-2400000, 1600000),
+#     ylim = c(200000, 2500000),
+#     expand = FALSE,
+#     datum = NA
+#   )
+# 
+# 
+# puerto <- ggplot(data = usa) +
+#   geom_sf(fill = "white") + 
+#   theme_void() + 
+#   # theme_void(base_family = "Crimson Pro")  +
+#   geom_sf(
+#     data = site,
+#     # shape = 21,
+#     size = 4, alpha=.7,
+#     aes(group=Case, 
+#         color=Status) 
+#   ) +
+#   guides(fill = F, shape=F, color=F) +
+#   coord_sf(
+#     crs = st_crs(3991),
+#     xlim = c(-50000, 1000000),
+#     ylim = c(0, 260000),
+#     expand = FALSE,
+#     datum = NA
+#   )
+# 
+# # puerto
+# 
+# hawaii  <- ggplot(data = usa) +
+#   geom_sf(fill = "white") +  
+#   theme_void() + 
+#   # theme_void(base_family = "Crimson Pro")  +
+#   geom_sf(
+#     data = site,
+#     # shape = 21,
+#     size = 4, alpha=.7,
+#     aes(group=Case, 
+#         color=Status) 
+#   )  + 
+#   transition_time(value)+ 
+#   # scale_fill_date(name = "Filing\nDate", 
+#   #                 low = "#0072B2",
+#   #                 high = "#D55E00") +
+#   guides(fill = F, shape=F, color=F) +
+#   coord_sf(
+#     crs = st_crs(4135),
+#     xlim = c(-161,-154),
+#     ylim = c(18, 23),
+#     expand = FALSE,
+#     datum = NA
+#   )
+# 
+# p <- mainland +
+#   annotation_custom(
+#     grob = ggplotGrob(alaska),
+#     xmin = -2750000,
+#     xmax = -2750000 + (1600000 - (-2400000)) / 2.5,
+#     ymin = -2450000,
+#     ymax = -2450000 + (2500000 - 200000) / 2.5
+#   ) +
+#   annotation_custom(
+#     grob = ggplotGrob(hawaii),
+#     xmin = -1250000,
+#     xmax = -1250000 + (-154 - (-161)) * 120000,
+#     ymin = -2450000,
+#     ymax = -2450000 + (23 - 18) * 120000
+#   ) +
+#   annotation_custom(
+#     grob = ggplotGrob(puerto),
+#     xmin = 750000,
+#     xmax = 750000 + (1000000 - (-50000)) / 2,
+#     ymin = -2250000,
+#     ymax = -2250000 + (260000 - 0) /2
+#   ) 
+# 
+# # p
+# animate(p, end_pause=10, height=4, width=6, units="in", res=150)
+# anim_save("~/Desktop/starbucks_filings.gif")
+
+# ggsave(here("content", "data",  "starbucks", "map.png"),
+#        height=6, width=10, type = "cairo",
+#        units="in", dpi=200)
+
+
+
+
+usa <- ne_states(returnclass = 'sf',
+                 country = c("united states of america", 
+                             "puerto rico"))
+
+# ESRI:102003
+crs_lower48 <- "+proj=aea +lat_1=29.5 +lat_2=45.5 +lat_0=37.5 +lon_0=-96 +x_0=0 +y_0=0 +ellps=GRS80 +datum=NAD83 +units=m +no_defs"
+# EPSG:3338
+crs_alaska <- "+proj=aea +lat_1=55 +lat_2=65 +lat_0=50 +lon_0=-154 +x_0=0 +y_0=0 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs "
+# ESRI:102007
+crs_hawaii <- "+proj=aea +lat_1=8 +lat_2=18 +lat_0=13 +lon_0=-157 +x_0=0 +y_0=0 +ellps=GRS80 +datum=NAD83 +units=m +no_defs"
+
+crs_pr <- "+proj=lcc +lat_1=18.43333333333333 +lat_2=18.03333333333333 +lat_0=17.83333333333333 +lon_0=-66.43333333333334 +x_0=200000 +y_0=200000 +ellps=GRS80 +units=m +no_defs"
+
+site <- as_tibble(sub_dt) %>% group_by(Case) %>% 
+  group_modify(~ add_row(.x, value=as.Date(Sys.Date()))) %>% 
+  # ungroup() %>% 
+  complete(Case, value=full_seq(value, 1)) %>% 
+  arrange(Case) %>% 
+  fill(Status:variable) %>% 
+  filter(!is.na(lat_jit))
 
 site <- st_as_sf(
-  unique(st_dt[!is.na(lat)], by = "Case"),
+  unique(site, by = c("Case", "Status")),
   coords = c("long_jit", "lat_jit"),
   crs = 4326,
   agr = "constant"
 )
 
-library(gganimate)
-# tween_sf()
+site <- site %>% st_transform(crs_lower48)
+us_states <- as(usa, "sf") %>%
+  st_transform(crs_lower48)# remove Puerto Rico
 
-# library(transformr)
-mainland <- ggplot(data = usa) +
+# now remove Alaska and Hawaii for lower 48, and calculate bounding box
+us_lower48 <- dplyr::filter(us_states, !fips %in% c("US02", "US15", "72033"))
+site_lower48 <- dplyr::filter(site, !State %in% c("HI", "AK", "PR"))
+bb <- st_bbox(us_lower48)
+
+place_geometry <- function(geometry, position, scale = 1, 
+                           center=st_centroid(geometry)) {
+  (geometry - center) * scale +
+    st_sfc(st_point(position))
+}
+
+# move Alaska
+us_alaska <- dplyr::filter(us_states, postal == "AK")
+us_alaska2 <- st_transform(us_alaska, crs_alaska)
+st_geometry(us_alaska2) <- place_geometry(
+  st_geometry(us_alaska2),
+  c(bb$xmin + 0.1*(bb$xmax - bb$xmin),
+    bb$ymin - 0*(bb$ymax - bb$ymin)), 
+  scale=.4
+)
+# we're cheating here by assigning a projection that isn't correct; we
+# need to do this because the final compound map needs to have a single
+# projection
+st_crs(us_alaska2) <- crs_lower48
+
+# move Hawaii
+us_hawaii <- dplyr::filter(us_states, postal == "HI")
+us_hawaii2 <- st_transform(us_hawaii, crs_hawaii)
+hi_center <- st_centroid(st_geometry(us_hawaii2))
+st_geometry(us_hawaii2) <- place_geometry(
+  st_geometry(us_hawaii2),
+  c(bb$xmin + 0.3*(bb$xmax - bb$xmin),
+    bb$ymin + 0.*(bb$ymax - bb$ymin))
+)
+# cheating, as before
+st_crs(us_hawaii2) <- crs_lower48
+
+site_hi <- dplyr::filter(site, State == "HI") %>% 
+  st_transform(crs_hawaii) 
+st_geometry(site_hi) <-
+  place_geometry(st_geometry(site_hi),
+                 center = hi_center,
+                 c(bb$xmin + 0.3*(bb$xmax - bb$xmin),
+                   bb$ymin + 0.*(bb$ymax - bb$ymin)))
+st_crs(site_hi) <- crs_lower48
+
+
+
+## Puerto Rico
+us_pr <- dplyr::filter(us_states, fips == "72033")
+us_pr2 <- st_transform(us_pr, crs_pr)
+st_geometry(us_pr2) <- place_geometry(
+  st_geometry(us_pr2),
+  c(bb$xmin + 0.5*(bb$xmax - bb$xmin),
+    bb$ymin + -0.1*(bb$ymax - bb$ymin)), 
+  scale=2
+)
+# cheating, as before
+st_crs(us_pr2) <- crs_lower48
+
+
+x3 <- rbind(us_lower48, us_alaska2, us_hawaii2, us_pr2)
+site_map <- rbind(site_lower48, site_hi)
+
+stats <- site %>% ungroup() %>% 
+  group_by(value, Status) %>% summarize(n=n()) %>%
+  select(value, Status, n)
+stats$geometry <- NULL
+
+
+site_map <- mutate(site_map, 
+                   label=case_when(
+                     Status == "Voted to Unionize" ~ "\uf6de", 
+                     Status == "Filed" ~ "\uf7b6",
+                     Status == "Withdrawn" ~ "\uf00d"
+                   ))
+
+site_map <- site_map %>% 
+  arrange(case_when(
+    Status == "Voted to Unionize" ~ 3, 
+    Status == "Filed" ~ 2,
+    Status == "Withdrawn" ~ 1
+  )) %>% ungroup() %>% 
+  mutate(Status = ordered(Status, levels=rev(c("Voted to Unionize", 
+                                               "Filed", "Withdrawn"))), 
+         label= ordered(label, levels=rev(c("\uf6de", "\uf7b6", "\uf00d") )))
+
+
+# font_add("font-awesome", regular = "~/Library/Fonts/Font Awesome 6 Free-Solid-900.otf")
+# tmp <- site_map[site_map$Case %in% names(which(table(site_map$Case) > 1)),]
+# tmp <- tmp[tmp$State == "NY",]
+mainland <- ggplot(data = x3) +
   geom_sf(fill = "white") + 
   theme_void() + 
   # theme_void(base_family = "Crimson Pro")  +
-  geom_sf(
-    data = site,
+  geom_sf_text(
+    family="font-awesome",
+    data = site_map,
     # shape = 21,
-    color = "orangered3",
-    size = 4, alpha=.7,
-    aes(group=Case) 
-    # aes(fill = size, shape=Election_Data)
-  ) +
-  transition_time(Date_Filed)+ 
-  shadow_mark() + 
-  enter_grow() +
-  # exit_recolor(color = "orangered3") +
-  ggtitle("Union Filings at Starbucks", subtitle = "Date: {frame_time}") + 
-  labs(caption = "Location is approximate based on cities. Each is jittered so overlapping cases are obvious.") +
-  coord_sf(
-    crs = st_crs(2163),
-    xlim = c(-2500000, 2500000),
-    ylim = c(-2300000,  730000)
-  ) +
-  NULL
-
-# mainland
-
-alaska <- ggplot(data = usa) +
-  geom_sf(fill = "white") + 
-  theme_void() + 
-  # theme_void(base_family = "Crimson Pro")  +
-  geom_sf(
-    data = site,
-    # shape = 21,
-    color = "orangered3",
-    size = 4, alpha=.7,
+    size = 4,
+    aes(label=label,
+        color=Status, 
+        alpha=Status)
     # aes(fill = size, shape=Election_Data)
   ) + 
-  guides(fill = F, shape=F) +
-  coord_sf(
-    crs = st_crs(3467),
-    xlim = c(-2400000, 1600000),
-    ylim = c(200000, 2500000),
-    expand = FALSE,
-    datum = NA
-  )
+  scale_alpha_manual(values = c("Voted to Unionize"= 1, 
+                                "Filed"=.5, 
+                                "Withdrawn"=.5)) + 
+  scale_color_manual(values = c("Voted to Unionize"= "black", 
+                                "Filed"= "orangered3", 
+                                "Withdrawn"="yellow3"))  + 
+  guides(color = 
+           guide_legend(override.aes = 
+                          list(label = 
+                                 c("\uf6de", "\uf7b6", "\uf00d")) ), 
+         alpha="none") + 
+  geom_text(x=2500000, y=-550000, aes(label=paste(n)), 
+             data=stats[stats$Status=="Withdrawn",]) + 
+  geom_text(x=2500000, y=-240000, aes(label=paste(n)), 
+            data=stats[stats$Status=="Filed",]) + 
+  geom_text(x=2500000, y=70000, aes(label=paste(n)), 
+            data=stats[stats$Status=="Voted to Unionize",]) + 
+  transition_manual(value)+
+  # shadow_mark() +
+  # enter_grow() +
+  # exit_recolor(color = "orangered3") +
+  # ggtitle("Union Filings at Starbucks", subtitle = "Date: {frame_time}") + 
+  labs(title="Union Filings at Starbucks", 
+       subtitle = "Date: {current_frame}",
+    caption = "Location is approximate based on cities. Each is jittered so overlapping cases are obvious.") +
+  # coord_sf(
+  #   crs = st_crs(2163),
+  #   xlim = c(-2500000, 2500000),
+  #   ylim = c(-2300000,  730000)
+  # ) +
+  theme(plot.caption.position = "plot") +
+  coord_sf(clip = 'off') + 
+  NULL
+animate(mainland, end_pause=10, height=4, width=6, units="in", res=150)
 
-
-puerto <- ggplot(data = usa) +
-  geom_sf(fill = "white") + 
-  theme_void() + 
-  # theme_void(base_family = "Crimson Pro")  +
-  geom_sf(
-    data = site,
-    # shape = 21,
-    color = "orangered3",
-    size = 4, alpha=.7,
-    # aes(fill = size, shape=Election_Data)
-  ) +
-  guides(fill = F, shape=F) +
-  coord_sf(
-    crs = st_crs(3991),
-    xlim = c(-50000, 1000000),
-    ylim = c(0, 260000),
-    expand = FALSE,
-    datum = NA
-  )
-
-# puerto
-
-hawaii  <- ggplot(data = usa) +
-  geom_sf(fill = "white") +  
-  theme_void() + 
-  # theme_void(base_family = "Crimson Pro")  +
-  geom_sf(
-    data = site,
-    # shape = 21,
-    color = "orangered3",
-    size = 4, alpha=.7,
-    # aes(fill = size, shape=Election_Data)
-  )  + 
-  # scale_fill_date(name = "Filing\nDate", 
-  #                 low = "#0072B2",
-  #                 high = "#D55E00") +
-  guides(fill = F, shape=F) +
-  coord_sf(
-    crs = st_crs(4135),
-    xlim = c(-161,-154),
-    ylim = c(18, 23),
-    expand = FALSE,
-    datum = NA
-  )
-
-p <- mainland +
-  annotation_custom(
-    grob = ggplotGrob(alaska),
-    xmin = -2750000,
-    xmax = -2750000 + (1600000 - (-2400000)) / 2.5,
-    ymin = -2450000,
-    ymax = -2450000 + (2500000 - 200000) / 2.5
-  ) +
-  annotation_custom(
-    grob = ggplotGrob(hawaii),
-    xmin = -1250000,
-    xmax = -1250000 + (-154 - (-161)) * 120000,
-    ymin = -2450000,
-    ymax = -2450000 + (23 - 18) * 120000
-  ) +
-  annotation_custom(
-    grob = ggplotGrob(puerto),
-    xmin = 750000,
-    xmax = 750000 + (1000000 - (-50000)) / 2,
-    ymin = -2250000,
-    ymax = -2250000 + (260000 - 0) /2
-  ) 
-
-# p
-animate(p, end_pause=10, height=4, width=6, units="in", res=150)
 anim_save("~/Desktop/starbucks_filings.gif")
 
-# ggsave(here("content", "data",  "starbucks", "map.png"),
-#        height=6, width=10, type = "cairo",
-#        units="in", dpi=200)
+
+# library(ggstar)
+# site_map <- mutate(site_map, 
+#        label=case_when(
+#          Status == "Voted to Unionize" ~ "\uf6de", 
+#          Status == "Filed" ~ "\uf7b6",
+#          Status == "Withdrawn" ~ "0"
+#        ))
+# 
+# 
+# tmp <- site_map %>% 
+#   arrange(case_when(
+#     Status == "Voted to Unionize" ~ 3, 
+#     Status == "Filed" ~ 2,
+#     Status == "Withdrawn" ~ 1
+#   )) %>% ungroup() %>% 
+#   mutate(Status = ordered(Status, levels=rev(c("Voted to Unionize", 
+#                                            "Filed", "Withdrawn"))), 
+#          label= ordered(label, levels=rev(c("\uf6de", "\uf7b6", "\uf00d") )))
+# 
+# mainland <- ggplot(data = x3) +
+#   geom_sf(fill = "white") + 
+#   theme_void() + 
+#   # theme_void(base_family = "Crimson Pro")  +
+#   geom_sf_text(
+#     family="font-awesome",
+#     data = filter(tmp, value==as.Date("2022-02-01")),
+#     # shape = 21,
+#     size = 4, 
+#     aes(label=label,
+#         color=Status, 
+#         alpha=Status)
+#     # aes(fill = size, shape=Election_Data)
+#   ) + 
+#   scale_alpha_manual(values = c("Voted to Unionize"= 1, 
+#                                 "Filed"=.5, 
+#                                 "Withdrawn"=.5)) + 
+#   scale_color_manual(values = c("Voted to Unionize"= "black", 
+#                                 "Filed"= "orangered3", 
+#                                 "Withdrawn"="yellow3"))  + 
+#   guides(color = 
+#            guide_legend(override.aes = 
+#                           list(label = 
+#                                  c("\uf6de", "\uf7b6", "\uf00d")) ), 
+#          alpha="none") 
+# 
+# mainland
+
 
