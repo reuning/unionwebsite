bcdf3bc9c33bd13ad22731187c4ff45755122508
Kevin <kevin.reuning@gmail.com>
2022-12-20 10:52:20 -0500

    fixed columns passed to tidygeocoder for sb

--- gen/scripts/Mapping.R
+++ gen/scripts/Mapping.R
@@ -277,7 +277,7 @@ if (any(is.na(full_dt$seached))) {
   done_dt <- full_dt[!is.na(seached)]
   
   to_code_dt <- tidygeocoder::geocode(
-    as.data.frame(to_code_dt[,1:Voters]),
+    as.data.frame(to_code_dt[,1:Didnt_Vote]),
     city = "City",
     state = "State",
     full_results = T,
