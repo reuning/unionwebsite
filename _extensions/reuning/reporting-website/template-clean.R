5a178b008bcb6d1a9a4509600126e928d1550b67
Kevin Reuning <kevin.reuning@gmail.com>
2024-01-05 22:16:49 -0500

    Adding quarto format

---
+++ _extensions/reuning/reporting-website/template-clean.R
@@ -0,0 +1,36 @@
+
+if (!require(yaml)) {
+    simpleError("Please install the yaml package")
+}
+
+
+template_files <- list.files(pattern = "*-template.qmd",
+                             recursive = TRUE,
+                             ignore.case = TRUE,  full.names = TRUE)
+
+if (length(template_files) == 0) {
+    stop("No template files found, skipping processing")
+}
+yml_files <- gsub("qmd$", "yml", template_files)
+
+if("keep-files" %in% names(yml_files)){
+    if(yml_files$keep_files %in% c("TRUE", "true", "True")) {
+        stop("Not data deleting files")
+    }
+}
+
+for (ii in seq_along(template_files)){
+
+    base_path <- dirname(yml_files[ii])
+    all_vars <- read_yaml(yml_files[ii])
+
+    items <- unlist(all_vars$items, recursive = FALSE)
+    for (jj in seq_along(items)){
+
+        if ("data" %in% names(items[[jj]])) {
+            file_out <- file.path(base_path, items[[jj]]$data$filename)
+            file.remove(file_out)
+        }
+
+    }
+}
---
+++ _extensions/reuning/reporting-website/template-expand.R
@@ -0,0 +1,54 @@
+if (!require(yaml)) {
+    simpleError("Please install the yaml package")
+}
+
+
+template_files <- list.files(pattern = "*-template.qmd",
+                             recursive = TRUE,
+                             ignore.case = TRUE,  full.names = TRUE)
+
+if (length(template_files) == 0) {
+    stop("No template files found, skipping processing")
+}
+yml_files <- gsub("qmd$", "yml", template_files)
+
+
+
+for (ii in seq_along(template_files)){
+
+    template <- readLines(template_files[ii])
+    base_path <- dirname(yml_files[ii])
+    all_vars <- read_yaml(yml_files[ii])
+
+    if ("data" %in% names(all_vars)) {
+        ## Loading data if is set
+        eval(parse(text = all_vars$data))
+    }
+
+    items <- unlist(all_vars$items, recursive = FALSE)
+    for (jj in seq_along(items)){
+        qmd_out <- template
+
+        if ("data" %in% names(items[[jj]])) {
+            eval(parse(text = paste0("tmp_df <- ", items[[jj]]$data$subset)))
+            file_out <- file.path(base_path, items[[jj]]$data$filename)
+            write.csv(tmp_df, file_out, row.names = FALSE)
+
+            qmd_out <- gsub("\\{\\{< template data_filename >\\}\\}",
+                             paste0("\"", items[[jj]]$data$filename, "\""),
+                             qmd_out)
+
+        }
+        vars <- items[[jj]]
+        vars$data <- NULL
+        for (kk in seq_along(vars)) {
+            qmd_out <- gsub(paste0("\\{\\{< template ",
+                                    names(vars)[kk], " >\\}\\}"),
+                             vars[[kk]], qmd_out)
+        }
+
+        file_out <- file.path(base_path, paste0(names(items)[[jj]], ".qmd"))
+        writeLines(qmd_out, file_out)
+    }
+
+}
