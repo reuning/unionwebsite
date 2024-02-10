c27092d447fa37db4d931eb4a7baf8522314798f
Kevin Reuning <kevin.reuning@gmail.com>
2024-02-09 21:15:49 -0500

    fixing names

--- content/write_yaml.r
+++ content/write_yaml.r
@@ -7,11 +7,12 @@ data <- read_yaml("states-template.yml")
 state.abb
 state.name
 for(ii in seq_along(state.abb)){
+    index <- gsub(" " , "_", tolower(state.name[ii]))
     tmp_list <- list(State=state.name[ii], 
         data=list(subset=paste0("data[data$State==\"", state.abb[ii], "\",]"),
                 filename=paste0(state.abb[ii], "_data.csv")))
 
-    data$items[[state.name[ii]]] <- tmp_list
+    data$items[[index]] <- tmp_list
 }
 
 write_yaml(data, "states-template.yml")
@@ -25,12 +26,13 @@ data <- read_yaml("states-template.yml")
 data$items <- NULL
 
 for(ii in seq_along(nationals)){
+    index <- gsub(" " , "_", tolower(nationals[ii]))
     tmp_list <- list("Union_Short"=nationals[ii], 
                     "Union_Long"=nationals[ii], 
         data=list(subset=paste0("data[data$National==\"", nationals[ii], "\",]"),
                 filename=paste0(nationals[ii], "_data.csv")))
 
-    data$items[[nationals[ii]]] <- tmp_list
+    data$items[[index]] <- tmp_list
 }
 
 setwd(here::here("content/data/union"))
