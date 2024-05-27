b0d75e7ab890cbeed634fc306f2443f9810e729b
Kevin Reuning <kevin.reuning@gmail.com>
2024-05-27 14:43:58 -0400

    switching to official cli

--- netlify/edge-functions/AI_block.ts
+++ netlify/edge-functions/AI_block.ts
@@ -3,11 +3,12 @@ import type { Context, Config } from "@netlify/edge-functions";
 export default async (request: Request, context: Context) => {
 
   const BLOCKED_USERAGENT = "ccbot|chatgpt-user|gptbot|google-extended|anthropic-ai|claudebot|omgilibot|omgili|facebookbot|diffbot|bytespider|imagesiftbot|cohere-ai";
+  const url = request.url
   const headers = request.headers
   const useragent = headers.get("User-Agent") || ""
 
   if (RegExp(BLOCKED_USERAGENT).test(useragent.toLowerCase())) {
-    console.log(`Blocked access from ${useragent}`)
+    console.log(`Blocked access to ${url} from ${useragent}`)
     return new Response("User-Agent blocked", {
       headers: { "content-type": "text/html" },
       status: 403,
